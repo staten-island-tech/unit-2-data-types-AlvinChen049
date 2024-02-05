b27ff94ea879c9f45f7363ead95666ee9e92a95d
AlvinChen049 <alvinxuc2789@nycstudents.net>
2024-02-05 12:01:31 -0500

    1

---
+++ list.py
@@ -0,0 +1,16 @@
+animals = ["Zebra", "Camel", "Ape"]
+#start count at 0, reference each element with []
+""" for animal in animals:
+    print(animal) """
+    
+for animal in animals:
+    if (animal == "Camel"):
+     print("wE'RE IN THE DESERT")
+
+#strings operate like lists
+    x = "Derek has a skill issue"
+print(x[0])
+y = x.upper()
+print(y)
+""" words_list = x.split("a")
+print (len(words_list)) """
